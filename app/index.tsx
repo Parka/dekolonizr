4058b873a8708875f29054b3a58981b35eacb4b5
Francisco Casanova Ferro <franciscocasanovaferro@gmail.com>
2025-05-11 17:24:54 -0300

    feat: add translation from English to Tagalog

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,17 @@ import { Text, StyleSheet } from "react-native";
 import { Camera, useCameraDevice, useCameraPermission, useFrameProcessor } from "react-native-vision-camera";
 import { useTextRecognition } from "react-native-vision-camera-text-recognition";
 import { TextRecognitionOptions } from "react-native-vision-camera-text-recognition/lib/typescript/src/types";
+import FastTranslator from 'fast-mlkit-translate-text';
+import { Worklets } from "react-native-worklets-core";
+
+FastTranslator.prepare({
+  source: 'English',
+  target: 'Tagalog',
+  downloadIfNeeded: true, // set to false if you want to download mannually
+});
 
 export default function Index() {
+
   const device = useCameraDevice('back')
   const { hasPermission, requestPermission } = useCameraPermission()
   useEffect(() => { if (!hasPermission) requestPermission() }, [hasPermission, requestPermission])
@@ -12,14 +21,23 @@ export default function Index() {
   const options: TextRecognitionOptions = { language: 'latin' };
   const { scanText } = useTextRecognition(options)
 
+  const translate = (blocks) => {
+    console.log(blocks)
+    blocks.forEach(block => FastTranslator.translate(block.blockText).then(result => console.log("TRANSLATED: ", result)).catch(error => console.error(error)))
+  }
+
+  const translateOnJS = Worklets.createRunOnJS(translate)
+
   const frameProcessor = useFrameProcessor((frame) => {
     'worklet'
     const data = scanText(frame)
-    Object.keys(data).length && console.log(data, 'data')
+    if (!Object.keys(data).length) return
+    translateOnJS(data.blocks)
   }, [])
 
   if (!hasPermission) return <Text>PermissionsPage</Text>
   if (device == null) return <Text>NoCameraDeviceError</Text>
+
   return (
     <Camera
       style={StyleSheet.absoluteFill}
